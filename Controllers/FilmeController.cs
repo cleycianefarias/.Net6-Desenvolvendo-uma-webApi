eb6d158e6c1beabe0f927485fa7acc64c5952675
Cleyciane Farias <fariascleycianedelima@gmail.com>
2023-01-11 16:42:39 -0300

    Entendendo o conceito

---
+++ Controllers/FilmeController.cs
@@ -0,0 +1,30 @@
+﻿using FilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : ControllerBase
+{
+    private static List<Filme> filmes = new List<Filme>();
+
+    [HttpPost]
+    public void AdicionaFilme([FromBody]Filme filme)
+    {
+        // O método abaixo funciona, porém não é um método eficiente
+        /*if (!string.IsNullOrEmpty(filme.Titulo) &&
+            !string.IsNullOrEmpty(filme.Genero) &&
+            filme.Duracao>=70){
+            filmes.Add(filme);
+            Console.WriteLine(filme.Titulo);
+            Console.WriteLine(filme.Duracao);
+        }*/
+
+        filmes.Add(filme);
+        Console.WriteLine(filme.Titulo);
+        Console.WriteLine(filme.Duracao);
+
+    }
+
+}
