d3d718dd1e68d7b5ac63eab8a4fcffabf8a6717f
Cleyciane Farias <fariascleycianedelima@gmail.com>
2023-01-20 12:45:55 -0300

    Recupera a lista de filmes no backend

--- Controllers/FilmeController.cs
+++ Controllers/FilmeController.cs
@@ -10,8 +10,10 @@ public class FilmeController : ControllerBase
     private static List<Filme> filmes = new List<Filme>();
 
     [HttpPost]
+    //inserindo filmes
     public void AdicionaFilme([FromBody]Filme filme)
     {
+        #region
         // O método abaixo funciona, porém não é um método eficiente
         /*if (!string.IsNullOrEmpty(filme.Titulo) &&
             !string.IsNullOrEmpty(filme.Genero) &&
@@ -20,11 +22,19 @@ public class FilmeController : ControllerBase
             Console.WriteLine(filme.Titulo);
             Console.WriteLine(filme.Duracao);
         }*/
+        #endregion
 
         filmes.Add(filme);
         Console.WriteLine(filme.Titulo);
         Console.WriteLine(filme.Duracao);
+    }
 
+
+    [HttpGet]
+    //recupera todos os filmes correspondentes
+    public IEnumerable<Filme> RecuperaFilme()
+    {
+        return filmes;
     }
 
 }
